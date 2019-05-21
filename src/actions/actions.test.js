07ccce5ef591533a002893cef3a7124d1328659e
Jarrett Kong <jarrettkong@me.com>
2019-05-20 19:52:48 -0600

    Add actions tests

---
+++ src/actions/actions.test.js
@@ -0,0 +1,59 @@
+import * as mockData from '../util/mockData';
+import * as actions from './index';
+
+describe('addList', () => {
+	const { mockList } = mockData;
+	it('should return an action object with type ADD_LIST and payload of a list object', () => {
+		const expected = {
+			type: 'ADD_LIST',
+			payload: { list: mockList }
+		};
+		const action = actions.addList(mockList);
+		expect(action).toEqual(expected);
+	});
+});
+
+describe('deleteList', () => {
+	it('should return an action object with type DELETE_LIST and payload of a list object', () => {
+		const expected = {
+			type: 'DELETE_LIST',
+			payload: { id: 101 }
+		};
+		const action = actions.deleteList(101);
+		expect(action).toEqual(expected);
+	});
+});
+
+describe('addItem', () => {
+	it('should return an action object with type ADD_ITEM and payload of an item object', () => {
+		const { mockItem } = mockData;
+		const expected = {
+			type: 'ADD_ITEM',
+			payload: { item: mockItem }
+		};
+		const action = actions.addItem(mockItem);
+		expect(action).toEqual(expected);
+	});
+});
+
+describe('toggleItem', () => {
+	it('should return an action object with type TOGGLE_ITEM and payload of an id', () => {
+		const expected = {
+			type: 'TOGGLE_ITEM',
+			payload: { id: 101 }
+		};
+		const action = actions.toggleItem(101);
+		expect(action).toEqual(expected);
+	});
+});
+
+describe('deleteItem', () => {
+	it('should return an action object with type DELETE_ITEM and payload of an id', () => {
+		const expected = {
+			type: 'DELETE_ITEM',
+			payload: { id: 101 }
+		};
+		const action = actions.deleteItem(101);
+		expect(action).toEqual(expected);
+	});
+});
---
+++ src/util/mockData.js
@@ -0,0 +1,11 @@
+export const mockItem = {
+	listId: 3,
+	content: 'Hello, world!',
+	complete: false
+};
+
+export const mockList = {
+	id: 101,
+	title: 'My List',
+	items: []
+};
