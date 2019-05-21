60bae412c3c3252b9f09fcf3d42d49ec05d6a378
Kevin Krato <kratok08@gmail.com>
2019-05-21 07:47:41 -0600

    Modify component structure

--- src/containers/ListCard/ListCard.jsx
+++ src/containers/ListCard/ListCard.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import "./ListCard.scss";
 
 class ListCard extends Component {
   render() {
     return (
-      <div>
-        <h3>ListCard</h3>
-      </div>
+      <article className="ListCard">
+        <ul>
+          <div>
+            <i class="material-icons">crop_square</i>
+            <li>Go Fishing</li>
+          </div>
+          <div>
+            <i class="material-icons">crop_square</i>
+            <li>Return the book to the library</li>
+          </div>
+          <div>
+            <i class="material-icons">crop_square</i>
+            <li>Fold my clothes</li>
+          </div>
+        </ul>
+      </article>
     );
   }
 }
