57f1ab59d41dcdb4f8490bfd03c4886b00f77665
Kevin Krato <kratok08@gmail.com>
2019-05-20 19:01:53 -0600

    Section child content

--- src/containers/ListArea/ListArea.jsx
+++ src/containers/ListArea/ListArea.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import ListInput from "../ListInput/ListInput";
 import ListCard from "../ListCard/ListCard";
-
+import "./ListArea.scss";
 class ListArea extends Component {
   render() {
     return (
-      <div>
-        <h3>ListArea</h3>
-        <ListInput />
-        <ListCard />
+      <div className="ListArea">
+        <div className="input-section">
+          <ListInput />
+        </div>
+        <div className="output-section">
+          <ListCard />
+        </div>
       </div>
     );
   }
