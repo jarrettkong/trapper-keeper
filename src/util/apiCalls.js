384a3e3bb8160b77630620c68c6372796d0c9bc6
Jarrett Kong <jarrettkong@me.com>
2019-05-23 14:45:52 -0600

    Add existing info to listInput on /notes/:id route

--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -9,3 +9,11 @@ export const addNewList = async list => {
 	}
 	return await res.json();
 };
+
+export const getListData = async id => {
+	const res = await fetch(`${process.env.REACT_APP_BASE_URL}/api/v1/lists/${id}`);
+	if(!res.ok) {
+		throw new Error('Could not fetch list with that id');
+	}
+	return await res.json();
+}
