86e81b631d5624d8979867c7d34f3906d0960b54
Jarrett Kong <jarrettkong@me.com>
2019-05-23 21:16:51 -0600

    Add button to /new-note route

--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -1,14 +1,17 @@
 import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import ListArea from '../../containers/ListArea/ListArea';
-import ListInput from '../../containers/ListInput/ListInput';
+// import ListInput from '../../containers/ListInput/ListInput';
 
 class HomeScreen extends Component {
 	render() {
 		return (
 			<Fragment>
-				<section className="input-section">
-					<ListInput />
-				</section>
+				{/* <section className="input-section"> */}
+				<Link to="/new-note">
+					<button>Add New List</button>
+				</Link>
+				{/* </section> */}
 				<ListArea />
 			</Fragment>
 		);
