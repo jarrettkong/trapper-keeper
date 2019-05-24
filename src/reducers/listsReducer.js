252efdef1ea547a8226ca2f8537b13fcd6a09ed6
Kevin Krato <kratok08@gmail.com>
2019-05-24 09:54:17 -0600

    Modify reducer case to DELETE_CARD

--- src/reducers/listsReducer.js
+++ src/reducers/listsReducer.js
@@ -1,15 +1,16 @@
 const initialState = [];
 
 export const listsReducer = (state = initialState, action) => {
-	const { type, payload } = action;
-	switch (type) {
-		case 'ADD_LISTS':
-			return payload.lists;
-		case 'ADD_LIST':
-			return [...state, payload.list];
-		case 'DELETE_LIST':
-			return state.filter(list => list.id !== payload.id);
-		default:
-			return state;
-	}
+  const { type, payload } = action;
+  switch (type) {
+    case "ADD_LISTS":
+      return payload.lists;
+    case "ADD_LIST":
+      return [...state, payload.list];
+    case "DELETE_CARD":
+      console.log("firing delete card reducer");
+      return state.filter(list => list.id !== payload.id);
+    default:
+      return state;
+  }
 };
