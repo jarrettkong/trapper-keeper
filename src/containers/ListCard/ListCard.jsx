8a9d45c4134df55a4d5cd48e4e81977917a1fdf3
Kevin Krato <kratok08@gmail.com>
2019-05-24 09:50:41 -0600

    Migrate in List pathing and add Edit|Delete buttons

--- src/containers/ListCard/ListCard.jsx
+++ src/containers/ListCard/ListCard.jsx
@@ -1,28 +1,55 @@
 import React, { Component } from "react";
 import ListItem from "../../components/ListItem/ListItem";
-
+import { Link } from "react-router-dom";
+//redux
+import { connect } from "react-redux";
+import { deleteCard } from "../../actions";
+import { deleteList } from "../../util/apiCalls";
 
 import "./ListCard.scss";
 
 class ListCard extends Component {
-  //for strikethrough, each list item will likely need an id that when the card is clicked,
-  //that card's state will update to reflect that it has been completed.
-  //backend - task complete, frontend - strikethrough
+  deleteThisCard = async () => {
+    console.log("card's id:", this.props.id);
+    try {
+      const res = await deleteList(this.props.id);
+      console.log(res);
+      this.props.This(this.props.id);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   render() {
     const { title, notes } = this.props;
-    
-  const displayNotes = notes.map( (note) => {
-    return <ListItem key={note.id} {...note} />
-  })
+
+    const displayNotes = notes.map(note => {
+      return <ListItem key={note.id} {...note} />;
+    });
     return (
       <article className="ListCard">
         <h3>{title}</h3>
-        <ul>
-         {displayNotes}
-        </ul>
+        <ul>{displayNotes}</ul>
+        {/* <button className="btn" onClick={this.deleteThisCard}>
+          Delete
+        </button> */}
+        <Link
+          to={`/notes/${this.props.id}`}
+          key={this.props.id}
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <button className="btn">Edit</button>
+        </Link>
       </article>
     );
   }
 }
 
-export default ListCard;
+export const mapDispatchToProps = dispatch => ({
+  deleteCard: id => dispatch(deleteCard(id))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(ListCard);
