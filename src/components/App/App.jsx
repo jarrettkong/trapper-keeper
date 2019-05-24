8e580ff0cfd0b565be1677da34fc81db1c5281ca
Jarrett Kong <jarrettkong@me.com>
2019-05-24 09:01:21 -0600

    Add history to /new-note path

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,21 @@
-import React from "react";
-import "./App.scss";
-import HomeScreen from "../HomeScreen/HomeScreen";
-import ListInput from "../../containers/ListInput/ListInput";
+import React from 'react';
+import './App.scss';
+import HomeScreen from '../HomeScreen/HomeScreen';
+import ListInput from '../../containers/ListInput/ListInput';
 
-import { Route } from "react-router-dom";
+import { Route } from 'react-router-dom';
 
 const App = () => {
-  return (
-    <main>
-      <Route path="/" component={HomeScreen} />
-      <Route path="/new-note" component={ListInput} />
-      <Route path="/notes/:id" component={({match, history}) => <ListInput history={history} id={match.params.id}/>} />
-    </main>
-  );
-}
+	return (
+		<main>
+			<Route path="/" component={HomeScreen} />
+			<Route path="/new-note" component={({ history }) => <ListInput history={history} />} />
+			<Route
+				path="/notes/:id"
+				component={({ match, history }) => <ListInput history={history} id={match.params.id} />}
+			/>
+		</main>
+	);
+};
 
 export default App;
